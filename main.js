db6228096ad13ff33e9d3dcd4bcf65f55f426f02
sudomig <mu_@tuta.io>
2022-11-19 11:50:12 -0500

    Html course section

---
+++ main.js
@@ -0,0 +1,7 @@
+//  CHANGE NAVBAR STYLES ON SCROLL
+
+window.addEventListener('scroll', () => {
+  document
+    .querySelector('nav')
+    .classList.toggle('window-scroll', window.scrollY > 0);
+});
