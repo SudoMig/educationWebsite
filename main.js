5b45e6019ec19e48bafbe5f71349a888aeb4cf24
sudomig <mu_@tuta.io>
2023-02-12 15:10:32 -0500

    homepage

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ window.addEventListener('scroll', () => {
     .querySelector('nav')
     .classList.toggle('window-scroll', window.scrollY > 0);
 });
+
+
+// SHOW/ HIDE FAQ ANSWER
+
+// const faqs = document.querySelectorAll('.faq')
+
+// faqs.forEach(faq => {
+//     faq.addEventListener('click', () => {
+//         faq.classList.toggle('open')
+
+//         // Change Icon
+//         const icon = faq.querySelector('.faq__icon svg')
+//         if(icon. === 'plus') {
+//             icon.className = 'minus'
+//         } else {
+
+//         }
+//     })
+// })
